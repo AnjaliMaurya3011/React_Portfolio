73ce4f7f4bf0742495888ec266fd1adf904b5af0
Anjali Maurya <anjalimaurya1130@gmail.com>
2024-10-11 20:20:29 +0530

    my portfolio

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import pdf from '../Components/pdf/resume.pdf' 
+import pdf from "./pdf/Anjali Maurya CV.pdf" 
 import hero from './data/hero.json'
 import Typed from "typed.js";
 
